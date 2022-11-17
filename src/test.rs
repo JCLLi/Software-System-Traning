6b5ef1c563e7ac5662d4c5070742de67c74de347
Jiacong Li <J.Li-101@student.tudelft.nl>
2022-11-17 13:43:45 +0100

    Add search function

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::io;
 use std::fs::{self, DirEntry};
+use std::ops::Range;
 use std::path::{Path, PathBuf};
 use regex::bytes::Regex;
 use crate::GrepResult;
@@ -31,15 +32,31 @@ pub fn ite(entries: &mut Vec<PathBuf>, path: &Path) -> io::Result<()>{
     Ok(())
 }
 pub fn search(entries: &mut Vec<PathBuf>, regex: Regex,) -> Result<(), Box<dyn Error>>{
+    let mut counter = 0;
     for i in 0..entries.len(){
+        let mut ranges: Vec<Range<usize>>= Vec::new();
         let content = fs::read_to_string(&entries[i])?;
-        println!("\nContent is: {}", content);
-
-        for line in content.lines() {
-            if line.contains(regex.as_str()) {
-                println!("line is: {}", line);
+        let contents_u8 = content.as_bytes();
+        println!("There are {} characters in all", contents_u8.len());
+        if regex.is_match(contents_u8){
+            counter += 1;
+            let mut grep_res = GrepResult{
+                path: entries[i].clone(),
+                content: contents_u8.to_vec(),
+                ranges,
+                search_ctr: counter,
+            };
+            for mat in regex.find_iter(contents_u8) {
+                grep_res.ranges.push(Range{start: mat.start(), end: mat.end()});
             }
+            for i in 0..grep_res.ranges.len(){
+                println!("This match starts at {} and ends at {}", grep_res.ranges[i].start, grep_res.ranges[i].end);
+            }
+
+
+
         }
+
     }
 
     Ok(())
