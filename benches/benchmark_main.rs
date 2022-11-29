3bfde290aca89fe8c47236d8d8326ddb620e9909
Jiacong Li <jiacongli@tudelft.nl>
2022-11-29 23:42:54 +0100

    add final benchmark

---
+++ benches/benchmark_main.rs
@@ -0,0 +1,24 @@
+//If you want to run this benchmark, go to Cargo.toml,
+//uncomment content of [lib] and [[bin]]
+//
+//
+
+
+// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+// use lib::call_main;
+// use log::LevelFilter;
+//
+//
+// pub fn benchmark(c: &mut Criterion){
+//     c.bench_function("benchmarking", |b| {
+//
+//         b.iter(|| {
+//             call_main()
+//         })
+//     });
+//
+// }
+//
+//
+// criterion_group!(benches, benchmark);
+// criterion_main!(benches);
